7f96b500059ebec140a7a0dcc40ed15cb2259ca4
Karen Molina <milhadasdepapel@gmail.com>
2019-05-13 11:38:38 -0500

    router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React, { Component } from 'react'
+import './index.css'
+import App from './App'
+import Login from './components/login'
+import { BrowserRouter, Route } from 'react-router-dom'
 
+class RouteIndex extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Route path='./' Component={App} />
+        <Route path='./login' Component={Login} />
+      </BrowserRouter>
+    )
+  }
+}
+
+export default RouteIndex
