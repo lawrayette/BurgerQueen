206cd817694a318aa9141ceb73bdb9689e72cc19
Karen Molina <milhadasdepapel@gmail.com>
2019-05-14 09:02:15 -0500

    editando App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
+import firebaseConfig from '../src/firebaseConf/firebaseConf'
+import Login from '../src/views/login'
 import './App.css'
+import Menu from '../src/views/Menu'
+import Router from './Router'
 
-function App () {
-  return (
-    <div>
-      <h2> Pedido  </h2>
-    </div>
-  )
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: {}
+    }
+  }
 }
 
 export default App
